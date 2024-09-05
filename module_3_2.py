26176b449477a56ed5f7e308c12f19045f39c444
alexander <molokan2007@mail.ru>
2024-09-05 16:50:04 +0300

    Update module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -1,13 +1,18 @@
 def send_email ( message , recipient , * ,  sender = "university.help@gmail.com" ):
-    if '@' in sender or recipient:
-        if  ".com" or ".ru" or ".net" in sender or recipient:
-            if recipient == sender:
-                print( "Нельзя отправить письмо самому себе!" )
-                return
-            if sender == "university.help@gmail.com":
-                print("Письмо успешно отправлено с адреса " ,sender," на адрес" , recipient ,".")
+    if '@' in str(sender) and '@' in str(recipient):
+        if  ".com" in str(sender) or ".ru" in str(sender) or ".net" in str(sender):
+            if  ".com" in str(recipient) or ".ru" in str(recipient) or ".net" in str(recipient):
+                if recipient == sender:
+                    print( "Нельзя отправить письмо самому себе!" )
+                    return
+                if sender == "university.help@gmail.com":
+                    print("Письмо успешно отправлено с адреса " ,sender," на адрес" , recipient ,".")
+                else:
+                    print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса " , sender , " на адрес" , recipient , ".")
             else:
-                print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса " , sender , " на адрес" , recipient , ".")
+                print("Невозможно отправить письмо с адреса ", sender, " на адрес", recipient, ".")
+        else:
+            print("Невозможно отправить письмо с адреса ", sender, " на адрес", recipient, ".")
     else:
         print("Невозможно отправить письмо с адреса ", sender, " на адрес", recipient, ".")
 
